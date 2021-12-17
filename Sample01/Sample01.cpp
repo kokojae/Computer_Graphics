9f8ca9046b4e55183f83067b2a7c7d992a6715a4
Ko Min Jae <54296776+kokojae@users.noreply.github.com>
2021-12-17 20:05:42 +0900

    Sample01 Done
    Sample01 프로젝트 완성.
    glut 사용을 위해서는
    1) C/C++ > 일반 > 추가 포함 디렉터리
    2) 링커 > 일반 > 추가 라이브러리 디렉터리
    3) 링커 > 입력 > 추가 종속성
    위와 같은 프로젝트 설정이 필요하다.
    이때 프로젝트와 lib, dll 파일의 Debug와 Release모드를 맞춰주어야한다.
    소스 내에서는 Windows.h를 꼭 추가해주어야하고, glutCreateWindow() 호출전에 glutInit()를 호출해주어야한다.
    마지막으로 glutInit()의 인자를 넘기기 위해 main함수에 인자를 추가하여 int main(int argc, char* argv[])와 같이 선언해야한다.

--- Sample01/Sample01.cpp
+++ Sample01/Sample01.cpp
@@ -1,6 +1,23 @@
+#include <Windows.h>
+#include <GL/glut.h>
 
-
-int main()
+void MyDisplay()
 {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glVertex3f(-0.5, -0.5, 0.0);
+	glVertex3f(0.5, -0.5, 0.0);
+	glVertex3f(0.5, 0.5, 0.0);
+	glVertex3f(-0.5, 0.5, 0.0);
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutCreateWindow("OpenGL Drawing Example");
+	glutDisplayFunc(MyDisplay);
+	glutMainLoop();
 	return 0;
 }
